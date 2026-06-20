1c25b460a4d3af951bf444ee6dbc58f248356074
soutbiaafnts <bianca.fontes.dev@gmail.com>
2026-06-20 13:58:55 -0300

    feat(models): create AppointmentModel

---
+++ app/Models/AppointmentModel.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class AppointmentModel extends Model
+{
+    protected $table            = 'appointments';
+    protected $primaryKey       = 'id';
+    protected $useAutoIncrement = true;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = true;
+    protected $protectFields    = true;
+    protected $allowedFields    = [
+        'pet_id',
+        'user_id',
+        'appointment_date',
+        'reason',
+        'diagnosis',
+        'prescription',
+        'notes',
+        'status'
+    ];
+
+    protected bool $allowEmptyInserts = false;
+    protected bool $updateOnlyChanged = true;
+
+    protected array $casts = [];
+    protected array $castHandlers = [];
+
+    // Dates
+    protected $useTimestamps = true;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    // Validation
+    protected $validationRules      = [];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert   = [];
+    protected $afterInsert    = [];
+    protected $beforeUpdate   = [];
+    protected $afterUpdate    = [];
+    protected $beforeFind     = [];
+    protected $afterFind      = [];
+    protected $beforeDelete   = [];
+    protected $afterDelete    = [];
+}
