45f18c1e19a14713806b1dfbc1896a77080cf80f
soutbiaafnts <bianca.fontes.dev@gmail.com>
2026-06-22 13:49:14 -0300

    style(read): add table view with paginate

--- app/Views/species/list.php
+++ app/Views/species/list.php
@@ -7,8 +7,63 @@
 
 <?= $this->section('content'); ?>
 
-<?= d($message) ?>
-<?= d($species) ?>
-<?= d($pager) ?>
+<div class="container py-4">
+
+    <div class="card">
+        <h5 class="card-header">
+            <?= $title ?>
+        </h5>
+        <div class="card-body">
+            <h5 class="card-title">Lista de <?= $title ?></h5>
+            <p class="card-text">Aqui você pode criar, visualizar, editar e até deletar as espécies!</p>
+            <a href="#" class="btn btn-secondary px-4">Nova Espécie</a>
+
+            <hr>
+            
+            <table class="table table-sm table-striped table-bordered w-100">
+                <thead class="table-light">
+                    <tr class="text-center">
+                        <th scope="col">#</th>
+                        <th scope="col">Nome</th>
+                        <th scope="col">Data de Criação</th>
+                        <th scope="col">Data de Atualização</th>
+                        <th scope="col">Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($species as $specie): ?>
+                        <tr class="align-middle text-center">
+                            <th scope="row">
+                                <?= $specie['id'] ?>
+                            </th>
+                            <td>
+                                <?= $specie['name'] ?>
+                            </td>
+                            <td>
+                                <?= $specie['created_at'] ?>
+                            </td>
+                            <td>
+                                <?= $specie['updated_at'] ?>
+                            </td>
+                            <td class="d-flex justify-content-center gap-2">
+                                <a href="#" class="btn btn-sm btn-info px-4">Editar</a>
+                                <a href="#" class="btn btn-sm btn-danger px-4">Deletar</a>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+            
+            <div class="container d-flex justify-content-center">
+                <ul class="pagination">
+                    <?= $pager->links('default') ?>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    
+
+</div>
 
 <?= $this->endSection('content'); ?>
