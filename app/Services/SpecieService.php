90cded83f0192d7ba93790269915872fded6b2dc
soutbiaafnts <bianca.fontes.dev@gmail.com>
2026-06-22 11:36:36 -0300

    feat(validation): add base validationSpecieData

--- app/Services/SpecieService.php
+++ app/Services/SpecieService.php
@@ -10,4 +10,33 @@ class SpecieService {
     public function __construct() {
         $this->specieModel = new SpecieModel();
     }
+
+    // ------- VALIDAÇÕES
+    private function validateSpecieData(array $data): array {
+        $validation = service('validation');
+
+        $validation->setRules([
+            'name' => 'required|min_length[3]|is_unique[species.name]',
+        ], [
+            'name' => [
+                'required' => 'Este campo é obrigatório.',
+                'min_length' => 'O nome precisa ter pelo menos 3 caracteres.',
+                'is_unique' => 'Espécie já cadastrada.'
+            ],
+        ]);
+
+        if (!$validation->run($data)) {
+            return [
+                'success' => false,
+                'message' => 'Verifique os campos.',
+                'invalidArgs' => $validation->getErrors(),
+                'errors' => null,
+            ];
+        }
+
+        return [
+            'success' => true,
+            'message' => 'Espécie criada com sucesso!',
+        ];
+    }
 }
