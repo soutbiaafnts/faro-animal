2b7aa46b0c346f26491074b34df021f5d52eb9da
soutbiaafnts <bianca.fontes.dev@gmail.com>
2026-06-23 22:20:58 -0300

    feat(read): add getAllBreeds to BreedService

--- app/Services/BreedService.php
+++ app/Services/BreedService.php
@@ -10,4 +10,25 @@ class BreedService {
     public function __construct() {
         $this->breedModel = new BreedModel();
     }
+
+    public function getAllBreeds() {
+        try {
+            $breeds = $this->breedModel->paginate(10);
+            $pager = $this->breedModel->pager;
+
+            return [
+                'success' => true,
+                'message' => 'Busca realizada com sucesso!',
+                'pager' => $pager,
+                'breeds' => $breeds,
+            ];
+        } catch (\Exception $e) {
+            return [
+                'success' => false,
+                'message' => 'Erro ao buscar raças.',
+                'invalidArgs' => [],
+                'errors' => $e->getMessage(),
+            ];
+        }
+    }
 }
