e6e453a30bffe7ce625f0809b1a0f8836ce4ce57
soutbiaafnts <bianca.fontes.dev@gmail.com>
2026-07-05 18:00:21 -0300

    feat(csrf): add csrf field for update breed form

--- app/Views/breeds/edit.php
+++ app/Views/breeds/edit.php
@@ -15,6 +15,8 @@
         </h5>
         <div class="card-body">
             <form action="<?= url_to('breeds.update', $breed['id']) ?>" method="post" class="row g-2 mx-auto">
+                <?= csrf_field() ?>
+
                 <input type="hidden" name="species_id" value="<?= $breed['species_id'] ?>">
                 <div class="col-md-6">
                     <label for="name" class="form-label">Nome</label>
@@ -26,8 +28,8 @@
 
                 <div class="col-md-6">
                     <label for="specie_id" class="form-label">Espécie</label>
-                    <select name="specie" id="specie_id"  class="form-select" disabled>
-                       <option selected value=""><?= esc($breed['specie_name']) ?></option>
+                    <select name="specie" id="specie_id" class="form-select" disabled>
+                        <option selected value=""><?= esc($breed['specie_name']) ?></option>
                     </select>
                 </div>
 
